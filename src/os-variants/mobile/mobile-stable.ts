4d75c84379836b81b684dbc593d2432c1f3d8149
Seshan Ravikumar <seshan10@me.com>
2023-04-27 15:58:24 -0400

    changes

--- src/os-variants/mobile/mobile-stable.ts
+++ src/os-variants/mobile/mobile-stable.ts
@@ -3,8 +3,7 @@ import { BUILD_DIR, ROOTFS_DIR, OUTPUT_DIR, FILES_DIR, arch, TARGET_DEVICE, PROL
 
 export async function buildMobileStable() {
     exec(`sudo arch-chroot ${ROOTFS_DIR} /bin/bash -x <<'EOF'
-        sudo pacman -R libpipewire pipewire pipewire-pulse wireplumber libpipewire --noconfirm 
-        yes | sudo pacman -S pipewire-media-session --noconfirm
+        yes | sudo pacman -S pipewire-media-session --noconfirm --ask 4
         echo "[danctnix]" >> /etc/pacman.conf
         echo 'Server = https://p64.arikawa-hi.me/$repo/$arch/' >> /etc/pacman.conf
         echo "SigLevel = Never" >> /etc/pacman.conf
