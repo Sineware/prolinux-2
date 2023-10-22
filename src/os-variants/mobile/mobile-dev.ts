ee34be210b34d0a1f1444450c587748b6544ab5f
EspiDev <espidev@gmail.com>
2023-10-22 00:31:51 -0400

    Switch plasma-mobile to master

--- src/os-variants/mobile/mobile-dev.ts
+++ src/os-variants/mobile/mobile-dev.ts
@@ -21,7 +21,8 @@ export async function buildMobileDev() {
         //["kwin", "master"]
         //["plasma-nm", "22377cf6"]
         //["knewstuff", "ec498e9"]
-        ["plasma-mobile", "work/devinlin/foliorewrite"],    ]
+        //["plasma-mobile", "work/devinlin/foliorewrite"],    
+    ]
 
     //const packagesToBuild = "kcmutils plasma5support kirigami-addons plasma-mobile plasma-pa plasma-nm qqc2-breeze-style"
     // ktextwidgets gpgme
