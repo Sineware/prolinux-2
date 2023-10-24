d150d0df69266b3c36f97d64ada3fcfc529c4049
Seshan Ravikumar <seshan10@me.com>
2023-10-24 17:12:06 -0400

    Add flathub by default

--- distro-files/layout/opt/prolinux-setup/start.sh
+++ distro-files/layout/opt/prolinux-setup/start.sh
@@ -3,5 +3,9 @@
 if [ ! -d /sineware/data/customization/etc/ssh ]; then
     mkdir -pv /sineware/data/customization/etc/ssh
     ssh-keygen -A -f /sineware/data/customization
+    systemctl restart sshd
 fi
+
+# switch to the user user and add flathub if it is not already added
+su - user -c "flatpak remote-add --if-not-exists --collection-id=org.flathub.Stable --gpg-import /opt/flatpak/flathub.gpg --user flathub /opt/flatpak/flathub.flatpakrepo"
 killall pbsplash
