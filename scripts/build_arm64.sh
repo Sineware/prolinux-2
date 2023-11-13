e5cb2c4a5777b2396a1b67732d36c7ceb3f4bd11
Seshan Ravikumar <seshan10@me.com>
2023-11-13 01:35:04 -0500

    testing4

--- scripts/build_arm64.sh
+++ scripts/build_arm64.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
-TARGET_DEVICE=pine64-pinephone,pine64-pinephonepro,pine64-pinebookpro,xiaomi-beryllium:tianma,oneplus-enchilada npm run build
+#TARGET_DEVICE=pine64-pinephone,pine64-pinephonepro,pine64-pinebookpro,xiaomi-beryllium:tianma,oneplus-enchilada npm run build
+TARGET_DEVICE=oneplus-enchilada npm run build
 
 ./scripts/unmount.sh
 
-TARGET_DEVICE=pine64-pinephone npm run gen-image
-TARGET_DEVICE=pine64-pinephonepro npm run gen-image
-TARGET_DEVICE=pine64-pinebookpro npm run gen-image
+#TARGET_DEVICE=pine64-pinephone npm run gen-image
+#TARGET_DEVICE=pine64-pinephonepro npm run gen-image
+#TARGET_DEVICE=pine64-pinebookpro npm run gen-image
 
-TARGET_DEVICE=xiaomi-beryllium:tianma npm run gen-image
+#TARGET_DEVICE=xiaomi-beryllium:tianma npm run gen-image
 TARGET_DEVICE=oneplus-enchilada npm run gen-image
