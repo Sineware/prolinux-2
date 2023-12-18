98cd9b6400d305d5e9fa654473096aaa29536221
Seshan Ravikumar <seshan10@me.com>
2023-12-18 03:53:52 -0500

    Add GPL head

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,21 @@ import { buildEmbeddedDev } from './os-variants/embedded/embedded-dev';
 import { buildMobileStable } from './os-variants/mobile/mobile-stable';
 import { buildMobileCommon } from './os-variants/mobile/mobile-common';
 
+// #  Copyright (C) 2023 Seshan Ravikumar
+// #
+// #  This program is free software: you can redistribute it and/or modify
+// #  it under the terms of the GNU General Public License as published by
+// #  the Free Software Foundation, either version 3 of the License, or
+// #  (at your option) any later version.
+// #
+// #  This program is distributed in the hope that it will be useful,
+// #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// #  GNU General Public License for more details.
+// #
+// #  You should have received a copy of the GNU General Public License
+// #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 console.log("Starting ProLinux build on " + new Date().toLocaleString());
 console.log("🍜 Go get your noodles, this may take a while!");
 
