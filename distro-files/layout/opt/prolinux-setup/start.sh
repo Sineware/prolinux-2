8909aec0dc2b3b69787a786baabb1486da5138e0
Seshan Ravikumar <seshan10@me.com>
2023-12-19 23:22:40 -0500

    fix flatpak install

--- distro-files/layout/opt/prolinux-setup/start.sh
+++ distro-files/layout/opt/prolinux-setup/start.sh
@@ -10,6 +10,6 @@ fi
 chmod 4755 /usr/bin/bwrap
 
 # offline install of flathub for default user
-su - user -c "flatpak remote-add --if-not-exists --collection-id=org.flathub.Stable --gpg-import /opt/flatpak/flathub.gpg --user flathub /opt/flatpak/flathub.flatpakrepo"
 cp /opt/flatpak/flathub.trustedkeys.gpg ~/.local/share/flatpak/repo/flathub.trustedkeys.gpg
+su - user -c "flatpak remote-add --if-not-exists --collection-id=org.flathub.Stable --gpg-import /opt/flatpak/flathub.gpg --user flathub /opt/flatpak/flathub.flatpakrepo"
 killall pbsplash
