74b33b2e817713a82b971ef4c0467b5dc716a296
Seshan Ravikumar <seshan10@me.com>
2024-02-18 20:25:49 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -137,7 +137,7 @@ async function main() {
         pacman -S --noconfirm --needed xorg
 
         # server related packages
-        pacman -S --noconfirm --needed podman podman-docker podman-dnsname netavark buildah
+        pacman -S --noconfirm --needed podman podman-docker netavark buildah
 
 
 
