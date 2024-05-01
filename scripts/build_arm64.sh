e094e598332caedd4f50102a22ed1b2926a5cf27
Seshan Ravikumar <seshan10@me.com>
2024-05-01 02:01:35 -0400

    Update build_arm64.sh

--- scripts/build_arm64.sh
+++ scripts/build_arm64.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
-TARGET_DEVICE=pine64-pinephone,pine64-pinephonepro,pine64-pinebookpro,xiaomi-beryllium:tianma,oneplus-enchilada npm run build
+TARGET_DEVICE=postmarketos-trailblazer,pine64-pinephone,pine64-pinephonepro,pine64-pinebookpro,xiaomi-beryllium:tianma,oneplus-enchilada npm run build
 
 ./scripts/unmount.sh
 
+
+TARGET_DEVICE=postmarketos-trailblazer npm run gen-image
 TARGET_DEVICE=pine64-pinephone npm run gen-image
 TARGET_DEVICE=pine64-pinephonepro npm run gen-image
 TARGET_DEVICE=pine64-pinebookpro npm run gen-image
