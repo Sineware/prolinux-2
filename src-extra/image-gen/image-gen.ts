7207d3974e28d885a4a3082e4fa1b09fe2ae3d6f
Seshan Ravikumar <seshan10@me.com>
2024-06-30 18:22:39 -0400

    Update image-gen.ts 20353024 img size

--- src-extra/image-gen/image-gen.ts
+++ src-extra/image-gen/image-gen.ts
@@ -84,7 +84,7 @@ const buildTargetStandardPMOSDeviceImage = (targetDevice: string) => {
                 mkdir -pv work
                 cd work
                 sudo abootimg -x ../boot.img
-                sudo sed -i "s/bootsize.*/bootsize = 19200000/g" bootimg.cfg
+                sudo sed -i "s/bootsize.*/bootsize = 25000000/g" bootimg.cfg
                 sudo sed -i "s/cmdline.*/cmdline = console=tty0 PMOS_NO_OUTPUT_REDIRECT/g" bootimg.cfg
                 echo "- Packing new boot image..."
                 sudo abootimg --create boot.img -f bootimg.cfg -k zImage -r ../initramfs
