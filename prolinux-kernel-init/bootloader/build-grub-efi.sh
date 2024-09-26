9826495e345fdbe84db76c11c5f54e40b8b5587b
Seshan Ravikumar <seshan10@me.com>
2024-09-26 21:19:06 +0800

    Switch to grub mkstandalone and move config into efi

--- prolinux-kernel-init/bootloader/build-grub-efi.sh
+++ prolinux-kernel-init/bootloader/build-grub-efi.sh
@@ -7,5 +7,6 @@ fi
 
 echo "Generating GRUB2 EFI file for $ARCH"
 
-grub2-mkimage -O ${ARCH}-efi -o grub${ARCH}.efi --config=grub.cfg -p /boot/grub efi_gop gfxterm font gfxmenu part_gpt part_msdos ext2 normal boot search search_label configfile echo loopback squash4 linux progress
+grub2-mkstandalone -O ${ARCH}-efi -o grub${ARCH}.efi --fonts="unicode" --locales="en@quot" --themes="" --modules="efi_gop gfxterm font gfxmenu part_gpt part_msdos ext2 normal boot search search_label configfile echo ls loopback squash4 linux progress sleep read test" "boot/grub/grub.cfg=grub.cfg"
+
 mv -v grub${ARCH}.efi ../output/
